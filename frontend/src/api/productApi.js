ea3ce2bbd587eef452a352c7a8e3a90a34bde573
Panther-gehu <pranavbisht66@gmail.com>
2026-07-05 12:42:53 +0530

    Update productApi.js

--- frontend/src/api/productApi.js
+++ frontend/src/api/productApi.js
@@ -1,6 +1,33 @@
 import api from "./axios";
 
+// Get all products
 export const getProducts = async () => {
   const response = await api.get("/products/");
+  return response.data;
+};
+
+// Get single product by ID
+export const getProductById = async (id) => {
+  const response = await api.get(`/products/${id}`);
+  return response.data;
+};
+
+export const searchProducts = async (name) => {
+  const response = await api.get(`/products/search?name=${name}`);
+  return response.data;
+};
+
+export const addProduct = async (product) => {
+  const response = await api.post("/products/", product);
+  return response.data;
+};
+
+export const deleteProduct = async (id) => {
+  const response = await api.delete(`/products/${id}`);
+  return response.data;
+};
+
+export const updateProduct = async (id, product) => {
+  const response = await api.put(`/products/${id}`, product);
   return response.data;
 };
