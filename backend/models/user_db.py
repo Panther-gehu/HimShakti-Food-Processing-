5343815c7cd3708506b1d116f7109fb0806aec8c
Panther-gehu <pranavbisht66@gmail.com>
2026-07-05 12:32:34 +0530

    Connecting backend and database

---
+++ backend/models/user_db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class UserDB(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False, index=True)
+    password = Column(String, nullable=False)
