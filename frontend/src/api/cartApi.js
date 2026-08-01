b7894bcc0e919c7aedd0f493e2d746e150ccfb53
Panther-gehu <pranavbisht66@gmail.com>
2026-08-01 20:53:06 +0530

    Update cartApi.js

--- frontend/src/api/cartApi.js
+++ frontend/src/api/cartApi.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import api from "./axios";
 
-const BASE_URL = "https://himshakti-food-processing.onrender.com/api/cart";
+const BASE_URL = "/cart";
 
 // Add to Cart
 export const addToCart = async (user_id, product_id, quantity) => {
-    const response = await axios.post(BASE_URL, {
+    const response = await api.post(BASE_URL, {
         user_id,
         product_id,
         quantity
@@ -15,13 +15,13 @@ export const addToCart = async (user_id, product_id, quantity) => {
 
 // Get Cart Items
 export const getCart = async () => {
-    const response = await axios.get(BASE_URL);
+    const response = await api.get(BASE_URL);
     return response.data;
 };
 
 // Update Quantity
 export const updateCart = async (id, user_id, product_id, quantity) => {
-    const response = await axios.put(`${BASE_URL}/${id}`, {
+    const response = await api.put(`${BASE_URL}/${id}`, {
         user_id,
         product_id,
         quantity
@@ -32,13 +32,13 @@ export const updateCart = async (id, user_id, product_id, quantity) => {
 
 // Delete Cart Item
 export const deleteCartItem = async (id) => {
-    const response = await axios.delete(`${BASE_URL}/${id}`);
+    const response = await api.delete(`${BASE_URL}/${id}`);
     return response.data;
 };
 
 // Checkout
 export const checkoutCart = async (userId) => {
-    const response = await axios.post(
+    const response = await api.post(
         `${BASE_URL}/checkout/${userId}`
     );
 
