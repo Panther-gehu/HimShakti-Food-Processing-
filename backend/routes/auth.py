273c97a2cdd45d40aa3402d2f9e47cd2db898bb3
Panther-gehu <pranavbisht66@gmail.com>
2026-08-01 11:00:25 +0530

    Update auth.py

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,19 +1,30 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Request
 from sqlalchemy.orm import Session
 
 from database import get_db
 from models.user_db import UserDB
 from models.user import UserSignup, UserLogin
 
+from auth import (
+    hash_password,
+    verify_password,
+    create_access_token
+)
+
+from limiter import limiter
+
 router = APIRouter(
     prefix="/api/auth",
     tags=["Authentication"]
 )
 
-
+# ==========================================
 # SIGNUP
+# ==========================================
 @router.post("/signup", status_code=201)
+@limiter.limit("5/minute")
 def signup(
+    request: Request,
     user: UserSignup,
     db: Session = Depends(get_db)
 ):
@@ -28,9 +39,11 @@ def signup(
             detail="Username already exists"
         )
 
+    hashed_password = hash_password(user.password)
+
     new_user = UserDB(
         username=user.username,
-        password=user.password
+        password=hashed_password
     )
 
     db.add(new_user)
@@ -39,29 +52,56 @@ def signup(
 
     return {
         "message": "User registered successfully",
-        "user": new_user
+        "user": {
+            "id": new_user.id,
+            "username": new_user.username
+        }
     }
 
 
+# ==========================================
 # LOGIN
+# ==========================================
 @router.post("/login")
+@limiter.limit("5/minute")
 def login(
+    request: Request,
     user: UserLogin,
     db: Session = Depends(get_db)
 ):
 
     existing_user = db.query(UserDB).filter(
-        UserDB.username == user.username,
-        UserDB.password == user.password
+        UserDB.username == user.username
     ).first()
 
-    if existing_user:
-        return {
-            "message": "Login successful",
-            "user": existing_user
-        }
+    if existing_user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password"
+        )
 
-    raise HTTPException(
-        status_code=401,
-        detail="Invalid username or password"
-    )
+    if not verify_password(
+        user.password,
+        existing_user.password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password"
+        )
+
+    access_token = create_access_token(
+    data={
+        "sub": existing_user.username,
+        "user_id": existing_user.id
+    }
+)
+
+    return {
+        "message": "Login successful",
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user": {
+            "id": existing_user.id,
+            "username": existing_user.username
+        }
+    }
